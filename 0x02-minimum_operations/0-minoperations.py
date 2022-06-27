0419f5b79639b745c149c5173a29eef32f323c88
Kenmind <kennedykalaluka04@gmail.com>
2022-06-27 13:59:18 +0200

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Defines minOperations"""
+
+
+def minOperations(n):
+    """Performs minOperations on n and
+    returns the total number of performed operations to achieve n"""
+    if not isinstance(n, int):
+        return 0
+
+    _times: int = 0
+    _copied: int = 0
+    _max: int = 1
+
+    while _max < n:
+        if _copied == 0:
+            _copied = _max
+            _max += _copied
+            _times += 2
+
+        elif (n - _max) > 0 and (n - _max) % _max == 0:
+            _copied = _max
+            _max += _copied
+            _times += 2
+
+        elif _copied > 0:
+            _max += _copied
+            _times += 1
+
+    return _times
